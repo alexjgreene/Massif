068e6cf82f7db0d0cf8589131df6bfdcfe0cca7f
alexjgreene <alex_greene@mail.ru>
2017-10-03 14:26:01 +0700

    Вывод среднего значения цен в массиве

---
+++ index (2).php
@@ -0,0 +1,11 @@
+		<div>
+		<?php
+		$prices=['120','130','150','130','130','120'];
+		$sum=0;
+		for ($i=0;$i<count($prices);$i++){
+			$sum+=$prices[$i];
+		}
+		$avg=$sum/count($prices);
+		echo $avg;
+		?>
+		</div>
